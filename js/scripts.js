eb06275e9d5a248876dc43a353df57cf64ed5e95
Kristin Brewer-Lowe and Paige Williams <paige@>
2018-10-29 14:50:59 -0700

    Add buttons to move task to different divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,76 @@
 //--- Business Logic for DestinationTracker
-function DestinationTracker() {
-  this.destinations = [];
+function ToDoList() {
+  this.tasks = [];
   this.currentId = 0
 }
 
-DestinationTracker.prototype.addDestination = function(destination) {
-  destination.id = this.assignId();
-  this.destinations.push(destination);
+ToDoList.prototype.addTask = function(task) {
+  task.id = this.assignId();
+  this.tasks.push(task);
 }
 
-DestinationTracker.prototype.assignId = function() {
+ToDoList.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
 
 // Business Logic for Destination
-function Destination(city, country, attractions, daysThere, dateVisited){
-  this.city = city,
-  this.country = country,
-  this.attractions = attractions,
-  this.daysThere = daysThere,
-  this.dateVisited = dateVisited
-
+function Task(title, description, type){
+  this.title = title,
+  this.description = description,
+  this.type = type
 }
 
 
-var destinationTracker = new DestinationTracker();
-var destination1 = new Destination("Portland", "USA", ["Rose Garden", "Bridges"], 3, "10/29/2018- 10/31/2018");
-var destination2 = new Destination("Seattle", "USA", ["Rose Garden", "Bridges"], 3, "10/29/2018- 10/31/2018");
-var destination3 = new Destination("SF", "USA", ["Rose Garden", "Bridges"], 3, "10/29/2018- 10/31/2018");
-var destination4 = new Destination("LA", "USA", ["Rose Garden", "Bridges"], 3, "10/29/2018- 10/31/2018");
-
-destinationTracker.addDestination(destination1);
-destinationTracker.addDestination(destination2);
-destinationTracker.addDestination(destination3);
-destinationTracker.addDestination(destination4);
-
-console.log(destinationTracker);
 // --- User Interface Logic
+function addToList(task){
+  $(".results").append("<li id='" + task.id + "'>" + task.title + " <button id='button" + task.id + "'>Mark complete</button></li>");
+  $("#" + task.id).click(function(event){
+    $(".title").text(task.title);
+    $(".description").text(task.description);
+    $(".type").text(task.type);
+    $(".details").toggle();
+  });
+  $("#button" + task.id).click(function(){
+    $(".completed-list").show();
+    $("#" + task.id).remove();
+    $(".completedResults").append("<li id='" + task.id + "'>" + task.title + " <button id='delete" + task.id + "'>Delete</button></li>");
+  });
+}
+
+
+
+
+var toDoList = new ToDoList();
+
 $(document).ready(function(){
+  $("#to-do").submit(function(event){
+    event.preventDefault();
+    var title = $("input#title").val();
+    var description = $("input#description").val();
+    var type = $("#type").val();
+    var task = new Task(title, description, type);
+    toDoList.addTask(task);
+    console.log(toDoList);
+    addToList(task);
 
-  for(var i = 0; i < destinationTracker.destinations.length; i ++){
-    var id = i;
-    var properties = "properties";
-    var propertyid = "properties" + i;
-    $("#destinations").after("<h2 id='" + id + "'>" + destinationTracker.destinations[i].city + "</h2>" +
-    "<ul id='" + propertyid + "' class='"+ properties +"'>" +
-    "<li><strong>Country: </strong>" + destinationTracker.destinations[i].country + "</li>" +
-    "<li><strong>Attractions: </strong>" + destinationTracker.destinations[i].attractions + "</li>" +
-    "<li><strong>Days spent there: </strong>" + destinationTracker.destinations[i].daysThere + "</li>" +
-    "<li><strong>Dates visited: </strong>" + destinationTracker.destinations[i].dateVisited + "</li>");
-  }
+    $()
 
-  $("#0").click(function(){
-    $("#properties0").toggle();
-  });
-  $("#1").click(function(){
-    $("#properties1").toggle();
-  });
-  $("#2").click(function(){
-    $("#properties2").toggle();
-  });
-  $("#3").click(function(){
-    $("#properties3").toggle();
+    $(".uncompleted-list").show();
   });
 
 });
+
+
+
+// for(var i = 0; i < destinationTracker.destinations.length; i ++){
+//   var id = i;
+//   var properties = "properties";
+//   var propertyid = "properties" + i;
+//   $("#destinations").after("<h2 id='" + id + "'>" + destinationTracker.destinations[i].city + "</h2>" +
+//   "<ul id='" + propertyid + "' class='"+ properties +"'>" +
+//   "<li><strong>Country: </strong>" + destinationTracker.destinations[i].country + "</li>" +
+//   "<li><strong>Attractions: </strong>" + destinationTracker.destinations[i].attractions + "</li>" +
+//   "<li><strong>Days spent there: </strong>" + destinationTracker.destinations[i].daysThere + "</li>" +
+//   "<li><strong>Dates visited: </strong>" + destinationTracker.destinations[i].dateVisited + "</li>");
+// }
